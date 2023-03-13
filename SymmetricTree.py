0f1b3541e9a4631447f8a07be030d272de78299b
borady <ali.ahmed@speedrent.com>
2023-03-13 13:39:02 +0100

    leetCode streak day 16 modif

--- SymmetricTree.py
+++ SymmetricTree.py
@@ -25,7 +25,7 @@ class Solution:
             if not t1 or not t2:
                 return False
             
-            return (t1.val == t2.val) and helper(t1.left,t2.right)
+            return (t1.val == t2.val) and helper(t1.left,t2.right) and helper(t1.right, t2.left)
         
         return helper(root, root)
         
